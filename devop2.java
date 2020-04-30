6c96f2d0295b9ee35a1bc82efaa31f6a88f19ebb
saniya <manchekarsaniya@gmail.com>
2020-04-30 18:35:34 +0530

    added Integration_BEIT

---
+++ devop2.java
@@ -0,0 +1,11 @@
+public class devop2{
+    public static void main(String[] args) {
+        for(int i=0; i<5; i++)
+
+        {
+          System.out.println("Integration of git with Jenkins");
+          }
+    }
+}
+
+
